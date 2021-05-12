47522cba3cb495912a87a661cdcccda5605ae0b9
kaarelp <kaarelpurde@gmail.com>
2021-05-12 17:47:54 +0300

    added more tests

--- src/test/java/com/kaarelp/MuseumVisitorsTest.java
+++ src/test/java/com/kaarelp/MuseumVisitorsTest.java
@@ -40,17 +40,53 @@ class MuseumVisitorsTest {
         MuseumVisitors m = new MuseumVisitors();
 
         List<VisitationPeriod> actual = m.findMaxVisitorsPeriod(singleVisitationStream());
-        System.out.println(actual.toString());
+
+        assertThat(actual, is(not(empty())));
+        assertThat(actual, hasSize(1));
+        VisitationPeriod found = actual.get(0);
+        assertEquals("00:00", found.getStartingMinuteIn24hFormat());
+        assertEquals("00:05", found.getEndingMinuteIn24hFormat());
+        assertEquals(1, found.getNrSimultaneousOfVisitors());
+    }
+
+    @Test
+    void findMaxVisitorsPeriod_singleVisitationCaseMinuteOverflow_found() {
+        MuseumVisitors m = new MuseumVisitors();
+
+        List<VisitationPeriod> actual = m.findMaxVisitorsPeriod(singleVisitationStreamCaseMinuteOverflow());
+
+        assertThat(actual, is(not(empty())));
+        assertThat(actual, hasSize(1));
+        VisitationPeriod found = actual.get(0);
+        assertEquals("00:00", found.getStartingMinuteIn24hFormat());
+        assertEquals("01:00", found.getEndingMinuteIn24hFormat());
+        assertEquals(1, found.getNrSimultaneousOfVisitors());
+    }
+
+    @Test
+    void findMaxVisitorsPeriod_twoLeftPartiallyOverlappingVisitations_found() {
+        MuseumVisitors m = new MuseumVisitors();
+
+        List<VisitationPeriod> actual = m.findMaxVisitorsPeriod(twoLeftPartiallyOverlappingVisitationsStream());
 
         assertThat(actual, is(not(empty())));
         assertThat(actual, hasSize(1));
         VisitationPeriod found = actual.get(0);
         assertEquals("13:00", found.getStartingMinuteIn24hFormat());
-        assertEquals("13:05", found.getEndingMinuteIn24hFormat());
+        assertEquals("13:03", found.getEndingMinuteIn24hFormat());
+        assertEquals(2, found.getNrSimultaneousOfVisitors());
     }
 
     private Stream<String> singleVisitationStream() {
-        return Stream.of("13:00,13:05");
+        return Stream.of("00:00,00:05");
+    }
+
+    private Stream<String> singleVisitationStreamCaseMinuteOverflow() {
+        return Stream.of("00:00,01:00");
+    }
+
+    private Stream<String> twoLeftPartiallyOverlappingVisitationsStream() {
+        return Stream.of("13:00,13:05", "13:00,13:03");
     }
 
 }
